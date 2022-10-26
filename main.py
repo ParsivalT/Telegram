77168b933e2bb822c51e3e1df57837d17c99e77f
Thiago Vinicius <tthiagovinnicius@gmail.com>
2022-10-26 20:08:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
 # Chat_Id 1512304430
+
 from datetime import datetime
-from classes import CoinGeckoAPI, TelegramBot, dolar
+from classes import CoinGeckoAPI, TelegramBot, dolar, LOG
 from time import sleep
-from rich.console import Console
 import schedule
 
 
-
-console = Console()
 API = CoinGeckoAPI(url_base='https://api.coingecko.com/api/v3')
 BOT = TelegramBot(token='5550528443:AAHD2RjvRdzvwRJIOlR0ieOUCgYBPxJUoeQ', chat_id=1512304430)
 
 def bot_telegram():
 
-    with console.status("[green]Processando... [/]") as status:
-
-        if API.ping():
-            console.log("[blue]API online[/]")
-            preco, atualizado_em = API.consulta_preco(id_moeda='bitcoin')
+   
+    if API.ping():
         
+        preco, atualizado_em = API.consulta_preco(id_moeda='bitcoin')
 
-            data_hora = datetime.fromtimestamp(atualizado_em).strftime('%x %X')
-            mensagem = list()
+        data_hora = datetime.fromtimestamp(atualizado_em).strftime('%x %X')
+        mensagem = list()
 
-            console.log("[blue]Consultando cotação dolar!![/]")
-            preço_dolar, variacao = dolar()
+        preço_dolar, variacao = dolar()
 
-            mensagem.append(f'*Ultimas Atualizações*')
-            mensagem.append(f'*Cotação do Dolar:* \n\t*Preço*: R$ {preço_dolar:.2f}' \
-                        f'\n\t*Variação de:* {variacao}%')
-            mensagem.append(f'*Cotação do Bitcoin*: \n\t*Preço*: R$ {preco:,.2f}' \
-                        f'\n\t*Horario*: {data_hora}\n\t')
+        mensagem.append(f'*Ultimas Atualizações*')
+        mensagem.append(f'*Cotação do Dolar:* \n\t*Preço*: R$ {preço_dolar:.2f}' \
+                    f'\n\t*Variação de:* {variacao}%')
+        mensagem.append(f'*Cotação do Bitcoin*: \n\t*Preço*: R$ {preco:,.2f}' \
+                    f'\n\t*Horario*: {data_hora}\n\t')
 
-            for mensagens in mensagem:
-                BOT.envia_mensagem(texto_markdown=mensagens)
+        for mensagens in mensagem:
+            BOT.envia_mensagem(texto_markdown=mensagens)
 
-            console.log("[blue]mensagem enviada com sucesso![/]")
-            
-            
-        else:
-            print('Api offline, Tente denovo mais tarde!')
+        LOG.info("Mensagem enviada!")
+        
+    else:
+        LOG.warning('Api offline, Tente denovo mais tarde!')
 
 
 schedule.every(5).minutes.do(bot_telegram)
